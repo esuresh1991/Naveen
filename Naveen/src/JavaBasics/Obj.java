ef4fbc4da7e598d0364c9c53860b321b4a89e3c0
esuresh1991 <esuresh1991@gmail.com>
2020-07-26 19:31:19 +0530

    dgc

---
+++ Naveen/src/JavaBasics/Obj.java
@@ -0,0 +1,14 @@
+package JavaBasics;
+
+public class Obj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Object a=1;
+		
+		System.out.println(a);
+
+	}
+
+}
